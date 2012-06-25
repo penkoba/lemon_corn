835b58f408f004b58ca9025d1307243e74e31528
Toshihiro Kobayashi <kobacha@mwa.biglobe.ne.jp>
2012-06-26 01:01:15 +0900

    added required include file to lemon_corn_data.c

--- core/lemon_corn_data.c
+++ core/lemon_corn_data.c
@@ -19,6 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+#include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
 #include <string.h>
